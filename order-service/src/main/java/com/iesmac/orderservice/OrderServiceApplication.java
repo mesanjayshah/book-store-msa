e1feacb247a505ea9fa561a9ec6b42a458c59db7
Sanjay Shah <messanjayshah31@gmail.com>
2025-03-13 11:06:34 +0545

    Fixed code formatting issues using Spotless

---
+++ order-service/src/main/java/com/iesmac/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.iesmac.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+//@EnableScheduling
+//@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
